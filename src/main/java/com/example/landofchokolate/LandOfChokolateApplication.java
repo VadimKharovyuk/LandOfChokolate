4a2780a05bbd7a7eab66e58c1cf1389af88984e0
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-07-13 22:20:25 +0300

    Initial commit

---
+++ src/main/java/com/example/landofchokolate/LandOfChokolateApplication.java
@@ -0,0 +1,13 @@
+package com.example.landofchokolate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandOfChokolateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandOfChokolateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/landofchokolate/LandOfChokolateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landofchokolate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandOfChokolateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
