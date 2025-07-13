062f8bae39ebdb2b2bddde5e4be9c24e79545d66
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-07-13 22:23:18 +0300

    add config

--- src/main/java/com/example/landofchokolate/LandOfChokolateApplication.java
+++ src/main/java/com/example/landofchokolate/LandOfChokolateApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LandOfChokolateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LandOfChokolateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LandOfChokolateApplication.class, args);
+    }
 
 }
