ccc73a29c8e804f07200c884f5b0abc680460787
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-07-20 02:00:03 +0300

    update CartCleanupScheduler

--- src/main/java/com/example/landofchokolate/enums/CartStatus.java
+++ src/main/java/com/example/landofchokolate/enums/CartStatus.java
@@ -1,10 +1,19 @@
 package com.example.landofchokolate.enums;
 
 
+import lombok.Getter;
 
+@Getter
 public enum CartStatus {
-    ACTIVE,     // Активная корзина
-    ABANDONED,  // Заброшенная корзина
-    CONVERTED,  // Корзина преобразована в заказ
-    EXPIRED     // Истёкшая корзина
+    ACTIVE("Активная корзина"),
+    ABANDONED("Заброшенная корзина"),
+    CONVERTED("Оформлен в заказ"),
+    EXPIRED("Истек срок действия");
+
+    private final String displayName;
+
+    CartStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
 }
