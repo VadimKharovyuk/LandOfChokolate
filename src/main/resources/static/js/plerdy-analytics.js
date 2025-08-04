8f28271befa5a637ff0cd7ddab71f3a933832e1d
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-08-04 14:19:04 +0300

    add contact#Id + plerdy-analytics.js

---
+++ src/main/resources/static/js/plerdy-analytics.js
@@ -0,0 +1,11 @@
+<!-- BEGIN PLERDY CODE -->
+<script type="text/javascript" defer data-plerdy_code='1'>
+    var _protocol="https:"==document.location.protocol?"https://":"http://";
+    _site_hash_code = "d9da29dc7b757af6358759cbf2fe0f73",_suid=63893, plerdyScript=document.createElement("script");
+    plerdyScript.setAttribute("defer",""),plerdyScript.dataset.plerdymainscript="plerdymainscript",
+    plerdyScript.src="https://a.plerdy.com/public/js/click/main.js?v="+Math.random();
+    var plerdymainscript=document.querySelector("[data-plerdymainscript='plerdymainscript']");
+    plerdymainscript&&plerdymainscript.parentNode.removeChild(plerdymainscript);
+    try{document.head.appendChild(plerdyScript)}catch(t){console.log(t,"unable add script tag")}
+</script>
+<!-- END PLERDY CODE -->
